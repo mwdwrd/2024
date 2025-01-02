e7259e570dbd7bf694f2eefec445a1a80d4b2d50
Matty Woodward <mwdwrd@gmail.com>
2025-01-02 18:49:10 -0500

    types tings

---
+++ app.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+
+declare module 'app' {
+  // Add any app-specific types here if needed
+} 
---
+++ global.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+
+declare module 'app' {}
+declare module 'components' {}
+declare module 'data' {}
+declare module 'lib' {}
+declare module 'public' {}
+declare module 'styles' {} 
